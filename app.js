6881b30dc9f8bb47c68a3c3f5cebb12527df265d
Tydre1 <oliveiraandre945@gmail.com>
2023-07-18 20:40:57 -0300

    mudando as cores e adicionando cores ao flipper

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#F15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#F15025", "rgba(95,168,214,1)", "rgba(201,95,214,1)", "yellow", "blue", "#ff0034", "rgba(238,174,202,1"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
-    // get random number between 0 -3 
+    // get random number between 0 - x 
     const randomNumber = getRandomNumber();
     console.log(randomNumber);
 
